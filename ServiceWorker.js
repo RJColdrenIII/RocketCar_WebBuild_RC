9026fe37b56e4049ea2cff3487fe698702d9cadd
RJColdrenIII <ronaldjcoldreniii@gmail.com>
2025-01-08 15:12:52 -0700

    Uploaded WebGL build files
    Uploaded Version 1.01 of game for online playability

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "RJColdren-RocketCarGame-0.1";
+const contentToCache = [
+    "Build/RocketCar_WebBuild_RC.loader.js",
+    "Build/RocketCar_WebBuild_RC.framework.js.unityweb",
+    "Build/RocketCar_WebBuild_RC.data.unityweb",
+    "Build/RocketCar_WebBuild_RC.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
